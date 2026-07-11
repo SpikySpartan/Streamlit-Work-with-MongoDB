82975d9dad88df0f88b89c3b22e1c968c58d212a
Awadhesh <awadheshkumarmahto77@gmail.com>
2026-07-11 09:00:32 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,6 +2,7 @@
 import base64
 import os
 import pymongo
+mongodb+srv://Awadhesh:<SwordArtOnline>@cluster0.gbpzk4b.mongodb.net/?appName=Cluster0
 conn = pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
 mydb =conn["Prime"]
 mycol = mydb["users"]
