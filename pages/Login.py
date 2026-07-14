58167d1432740d6ec619669127a024f80311b1ba
Awadhesh <awadheshkumarmahto77@gmail.com>
2026-07-14 21:24:58 +0530

    Update Login.py

--- pages/Login.py
+++ pages/Login.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pymongo
-conn = pymongo.MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.8.2")
+conn = pymongo.MongoClient("mongodb+srv://Awadhesh:sword@cluster0.gbpzk4b.mongodb.net/?appName=Cluster0")
 mydb =conn["Prime"]
 mycol = mydb["users"]
 st.set_page_config(
